104db03d4a0e0c03555ad5f1e21676d81aeb5788
happydilraj <happydilraj1022@gmail.com>
2024-04-10 18:29:55 +0530

    dilraj

--- src/Components/Auth/Signup.jsx
+++ src/Components/Auth/Signup.jsx
@@ -1,32 +1,41 @@
 import React, {useState} from 'react'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from './Firebase'
+import {useNavigate} from 'react-router-dom'
 
 const Signup = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate()
 
-    const handleSignIn = () => {
-        try {
-            createUserWithEmailAndPassword(auth, email, password)
-        } catch (error) {
-            console.log(error)
-        }
+    const handleSignIn = async (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password).then(()=>{
+            alert("Account sucessfully created!!")
+            navigate('/')
+        }).catch((err) => {
+            console.log(err)
+        })
+        
     }
 
   return (
-    <div className= 'mt-5 p-3 container border w-25'>
+    <div className= 'p-3 container border border-danger w-25' style={{marginTop: '10%'}}>
         <form onSubmit={handleSignIn}>
             <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <label htmlFor="exampleInputEmail1" className="form-label text-danger">Email address</label>
                 <input type="email" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)}/>
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <label htmlFor="exampleInputPassword1" className="form-label text-danger">Password</label>
                 <input type="password" className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </div>
-            <button type="submit" className="btn btn-primary">Sign Up</button>
+            <button type="submit" className="btn btn-danger">Sign Up</button>
             </form>
+            <div className='mt-5'>
+                <p className='text-danger'>Already have an account</p>
+                <button type="submit" className="btn btn-danger" onClick={() => navigate('/Login')}>Log In</button>
+            </div>
         </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { signOut } from 'firebase/auth'
+import { auth } from './Auth/Firebase'
+
 
 const Navbar = () => {
     let navigate = useNavigate()
+    const location = useLocation()
+
+    const handleLogout =  () => {
+       signOut(auth).then(()=>{
+        navigate('/')
+       }).catch((err)=>{
+        console.log(err)
+       })
+    }
+
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Dekh lenge</a>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand text-danger" href="#">Code-Wars 🚀</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                <a className="nav-link active text-danger" aria-current="page" href="#">Home</a>
                 </li>
             </ul>
-            <button className='btn btn-danger mx-1' onClick={()=> navigate('/Login')}>Login</button>
-            <button className='btn btn-danger mx-1' onClick={()=> navigate('/Signup')}>Sign up</button>
+            {
+              location.pathname==='/Main'?
+              <button className='btn btn-danger mx-1' onClick={()=> handleLogout()}>Log Out</button>
+              :
+              <>
+              <button className='btn btn-danger mx-1' onClick={()=> navigate('/Login')}>Login</button>
+              <button className='btn btn-danger mx-1' onClick={()=> navigate('/Signup')}>Sign up</button>
+              </>
+            }
             </div>
         </div>
         </nav>
